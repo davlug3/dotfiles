95456716458c0c769d6b81b51eba5e5d19cb60e6
Dave <112188845+davlug3@users.noreply.github.com>
2024-08-14 14:48:18 +0800

    tmux configurations

---
+++ install_tpm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Directory where TPM will be installed
+TPM_DIR="$HOME/.tmux/plugins/tpm"
+
+# Check if TPM directory exists, if not clone TPM repository
+if [ ! -d "$TPM_DIR" ]; then
+  git clone https://github.com/tmux-plugins/tpm $TPM_DIR
+fi
+
+# Source TPM in .tmux.conf
+echo "set -g @tpm_plugins 'tpm'" >> ~/.tmux.conf
+
