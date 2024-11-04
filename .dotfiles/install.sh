984de69e80fe6bdcae4413de85d63e149e5635d6
Dave <112188845+davlug3@users.noreply.github.com>
2024-11-04 13:42:05 +0800

    debugsss

--- .dotfiles/install.sh
+++ .dotfiles/install.sh
@@ -40,7 +40,7 @@ else
 	bash "$(pwd)/install_git.sh" || { echo "install_git.sh failed."; exit 1; } 
     else
         echo "install_git.sh not found. Downloading..."
-	curl -o "$(pwd)/install_git.sh" "http://cdn.jsdelivr.net/gh/$REPO_NAME/.dotfiles@git/install_git.sh" || {
+	curl -o "$(pwd)/install_git.sh" "https://cdn.jsdelivr.net/gh/$REPO_NAME@git/.dotfiles/install_git.sh" || {
             echo "Failed to download install_git.sh"
             exit 1
         }
@@ -61,21 +61,19 @@ if [ -f "$(pwd)/install_dotfiles.sh" ]; then
 	bash "$(pwd)/install_dotfiles.sh"
 else
 	echo "install_dotfiles.sh not found. Downloading..."
-	curl -O "http://cdn.jsdelivr.net/gh/$REPO_NAME/.dotfiles@git/install_dotfiles.sh" || {
+	curl -o "$(pwd)/install_dotfiles.sh" "https://cdn.jsdelivr.net/gh/$REPO_NAME@git/.dotfiles/install_dotfiles.sh" || {
 	    echo "Failed to download install_dotfiles.sh";
 	    exit 1;
 	}
-	echo "downloaded. $(pwd)"
-	bash "install_dotfiles.sh" || {
-	    echo "install_dotfiles.sh failed"
-	    exit 1
-	}
+	echo "downloaded..."
+
+	bash "$(pwd)/install_dotfiles.sh" || { echo "install_dotfiles.sh failed"; exit 1; }
 	rm "$(pwd)/install_dotfiles.sh"
 fi
 
 echo "------------"
-echo $(pwd)
 
+cd $HOME/.dotfiles
 bash ./install_tmux.sh || { echo "install_tmux.sh failed"; exit 1; }
 bash ./install_tpm.sh || { echo "install_tpm.sh failed"; exit 1; }
 
--- .dotfiles/install_dotfiles.sh
+++ .dotfiles/install_dotfiles.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 echo "Installing your dotfiles..."
-
-
 cd ~ || { echo "Failed to change to home directory."; exit 1; }
 git init || { echo "Failed to initialize Git repository."; exit 1; }
-git remote add origin ${DOTFILES_URL} | { echo "Failed to add remote repository."; exit 1; }
+git remote add origin ${DOTFILES_URL} 
 git fetch origin || { echo "Failed to fetch from remote repository."; exit 1; }
 git checkout -f git || { echo "Failed to checkout to git branch."; exit 1; }
 echo "Successfully installled dotfiles"
