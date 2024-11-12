e153efe6cb7148aefe574225e437a2e6e5ab499f
Dave <davelugtu1@gmail.com>
2024-11-12 11:50:01 +0800

    bugsss

--- setup.sh
+++ setup.sh
@@ -6,13 +6,13 @@ BACKUP_DIR=$HOME/.config_backup
 DOTFILES_HOME=$HOME/.config_dotfiles
 backup_and_link() {
 
-	# if backup dir doesnt exist, create it
-	if [ ! -d "$BACKUP_DIR" ]; then 
-		mkdir -p "$BACKUP_DIR"
-	fi
 
 	# check if file in $1 exist, if yes, move it
 	if [ -e "$HOME/$1" ]; then
+		# if backup dir doesnt exist, create it
+		if [ ! -d "$BACKUP_DIR" ]; then 
+			mkdir -p "$BACKUP_DIR"
+		fi
 		mv "$HOME/$1" "$BACKUP_DIR"
 	fi
 
